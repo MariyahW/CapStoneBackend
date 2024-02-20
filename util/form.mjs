9865f4cde239ce48fdcfaa250fd8a1d73fe111b7
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-02-19 21:12:41 -0500

    working on js for forms

--- util/form.mjs
+++ util/form.mjs
@@ -1,6 +1,26 @@
-let form =document.getElementById('form');
+let form = document.getElementById("formAdd");
+let update = document.getElementById("toUpdate");
+let name = document.getElementById("name1");
+let email = document.getElementById("email");
+let phone = document.getElementById("phone");
+let selection = document.getElementById("contactReason");
+let other = document.getElementById("otherText");
 
-form.addEventListener('submit', (event)=>{
+update.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const id2 = new formData(update);
+  const res = Object.fromEntries(id2);
+  const send = Json.stringify(res);
+
+  fetch(`http//localhost:3000/contact/${send}`).then(function fill(response) {
+    name.innerText = response.name;
+    email.innerText = response.email;
+    phone.innerText = response.phone;
+    selection.value = response.contactReason;
+    other.innerText = response.textBox;
+  });
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = new FormData(form);
 
@@ -21,7 +41,32 @@ form.addEventListener('submit', (event)=>{
       .catch(function (response) {
         //handle error
         console.log(response);
-    //
-    });
+        //
+      });
+  });
+});
 
-})
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+
+  const res = Object.fromEntries(formData);
+  const newBody = JSON.stringify(res);
+  console.log(formData);
+  console.log(res);
+  console.log(newBody);
+  fetch("http://localhost:3000/contact", {
+    method: "post",
+    body: newBody,
+    headers: { "Content-Type": "application/json" },
+  })
+    .then(function (response) {
+      //handle success
+      console.log(response);
+    })
+    .catch(function (response) {
+      //handle error
+      console.log(response);
+      //
+    });
+});

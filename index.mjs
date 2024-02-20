f338f83a936af40da6f66d1b2a963ccc86343e4c
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-02-20 17:58:20 -0500

    added http server

--- index.mjs
+++ index.mjs
@@ -5,6 +5,8 @@ import mongoose from 'mongoose';
 import bodyParser from "body-parser";
 const app = express();
 import cors  from "cors";
+import * as http from 'http'
+
 
 const PORT= process.env.PORT;
 
@@ -16,14 +18,16 @@ const corsOptions ={
   credentials: true,
   optionSuccessStatus:200
 }
-
+const server = http.createServer();
+server.on('request',app)
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/contact', contact);
 
 
+
 app.get('/', (req, res)=>res.json("Server is running...use /contact"))
 app.get('/contact')
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port: ${process.env.PORT}`);
   })
